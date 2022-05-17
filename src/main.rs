29a16bb5d284af10866adb4f71496b9d3e9b8d20
Jacob Shaffer <1503954+Phyllostachys@users.noreply.github.com>
2022-05-17 14:48:47 -0400

    add special processing for youtube urls to add time stamp

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,46 @@
-use std::io::prelude::*;
+use std::io::BufRead;
 use std::path::PathBuf;
+use std::thread;
+use std::time::Duration;
 
-use structopt::StructOpt;
 use rayon::prelude::*;
 use select::{document::Document, predicate::Name};
+use structopt::StructOpt;
+use tokio::runtime::Runtime;
+use ytextract::video;
 
 #[derive(Clone)]
 struct UrlTitle {
     url: String,
     title: String,
+}
+
+impl UrlTitle {
+    pub fn new(url: &String) -> UrlTitle {
+        UrlTitle {
+            url: String::from(url),
+            title: String::new(),
+        }
+    }
+
+    pub fn process_yt(&mut self, client: &ytextract::Client) {
+        if let Some(index) = self.url.find("v=") {
+            let (_, video_id) = self.url.split_at(index + 2);
+            let video_id: video::Id = match video_id.parse() {
+                Ok(i) => i,
+                Err(_) => return,
+            };
+            let rt = Runtime::new().unwrap();
+            let video = match rt.block_on(client.video(video_id)) {
+                Ok(v) => v,
+                Err(_) => return,
+            };
+            self.title
+                .push_str(format!("({})", video.date().to_string()).as_str());
+            self.title.push_str(" ");
+            self.title.push_str(video.title());
+        }
+    }
 }
 
 #[derive(StructOpt, Debug)]
@@ -26,42 +58,35 @@ struct Options {
 
 fn main() {
     let opt = Options::from_args();
-
-    let file = match std::fs::File::open(opt.input) {
-        Ok(f) => f,
-        Err(e) => {
-            panic!("{}", e);
-        }
-    };
-
+    let file = std::fs::File::open(opt.input).unwrap();
+    let client = ytextract::Client::new();
     let buf = std::io::BufReader::new(file);
-    let mut urls: Vec<UrlTitle> = Vec::new();
-    for line in buf.lines() {
-        match line {
-            Ok(l) => urls.push(UrlTitle {
-                url: l,
-                title: String::new(),
-            }),
-            Err(e) => println!("Error on getting line... {}", e),
-        }
-    }
+    let mut urls: Vec<String> = buf
+        .lines()
+        .filter_map(|line| line.ok())
+        .map(|url| String::from(url))
+        .collect();
 
     for group in urls.chunks_exact_mut(opt.chunk_size) {
-        let result: Vec<_> = group
+        let result: Vec<UrlTitle> = group
             .par_iter()
             .map(|url| {
-                let res = reqwest::blocking::get(String::from(&url.url)).unwrap();
-                let content = res.text().unwrap();
-                let document = Document::from(content.as_str());
-                UrlTitle {
-                    url: String::from(&url.url),
-                    title: if let Some(node) = document.find(Name("title")).next() {
-                        //url.title = node.text();
-                        node.text()
-                    } else {
-                        //url.title = String::new();
-                        String::new()
-                    },
+                if url.contains("youtube") {
+                    let mut ut = UrlTitle::new(url);
+                    ut.process_yt(&client);
+                    ut
+                } else {
+                    let res = reqwest::blocking::get(url).unwrap();
+                    let content = res.text().unwrap();
+                    let document = Document::from(content.as_str());
+                    UrlTitle {
+                        url: String::from(url),
+                        title: if let Some(node) = document.find(Name("title")).next() {
+                            node.text()
+                        } else {
+                            String::new()
+                        },
+                    }
                 }
             })
             .collect();
@@ -70,6 +95,6 @@ fn main() {
             println!("{} --- {}", url.url, url.title);
         }
 
-        std::thread::sleep(std::time::Duration::from_secs_f32(opt.delay_time));
+        thread::sleep(Duration::from_secs_f32(opt.delay_time));
     }
 }
